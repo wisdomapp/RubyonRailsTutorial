aa187e9a7d6f7fac943c2ee120bfc7d9b2d0c338
DESKTOP\wisdom <sora5773@gmail.com>
2018-01-16 19:45:23 +0900

    伝言板

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get'dengonban',to:'dengonban#index'
+  post'dengonban',to:'dengonban#index'
+  get 'dengonban/index'
+  post'dengonban/index'
   get 'helo/index'
   get 'helo',to:'helo#index'
   get 'helo/other'

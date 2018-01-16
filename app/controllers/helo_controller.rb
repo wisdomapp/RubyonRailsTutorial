9b1c5a9e2166485f9077fdf0f156232ee144c2dd
DESKTOP\wisdom <sora5773@gmail.com>
2018-01-16 16:37:30 +0900

    2-5

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,16 +1,11 @@
 class HeloController < ApplicationController
+    layout 'helo'
 
     def index
-        if request.post? then
-            @title='Result'
-            if params['r1']!= nil then
-                @msg='you swlected:'+params['r1']
-            else
-                @msg='not selected...'
-            end
-        else
-            @title='Index'
-            @msg='select radio button...'
-        end
+        @header='layout sample'
+        @footer='copyright sora'
+        @title='New Layout'
+        @msg='this is sample page'
     end
+
 end
